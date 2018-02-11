2934b4d9b174e738a780946075100258dcff6430
Bișoc George <fraizeraust99@gmail.com>
2018-02-11 10:18:14 +0100

    [CS2D-Scripts] Declare the Begging table as local + some coding style changes

--- Sample Scripts/Begging.lua
+++ Sample Scripts/Begging.lua
@@ -1,12 +1,12 @@
-Begging = {"pls me mod","i want adm pls"}
+local Begging = {"pls me mod", "i want adm pls"}
 
 function _say(id, text)
 	for i = 1, #Begging do
-		if (string.find(text,Begging[i])) then
-			msg("\169240128128"..player(id,"name").." was begging for administration ranks. Kicked!")
-			parse("kick "..id.." \"You've begged for administration ranks, therefore you're punished with a kick!\"")
+		if ( string.find(text,Begging[i]) ) then
+			msg("\169240128128" .. player(id, "name") .. " was begging for administration ranks. Kicked!")
+			parse("kick " .. id .. " \"You've begged for administration ranks, therefore you're punished with a kick!\"")
 		end
 	end
 end
 
-addhook("say","_say")
+addhook("say", "_say")
