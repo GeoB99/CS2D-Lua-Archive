a43c66f7c21618557cb32ce39b3a477e2209d408
Bișoc George <fraizeraust99@gmail.com>
2018-02-11 10:12:18 +0100

    [CS2D-Scripts] Sample of begging Lua script uploaded

---
+++ Sample Scripts/Begging.lua
@@ -0,0 +1,13 @@
+Begging = {"pls me mod","i want adm pls"}
+
+
+function _say(id, text)
+	for i = 1, #Begging do
+		if (string.find(text,Begging[i])) then
+			msg("\169240128128"..player(id,"name").." was begging for administration ranks. Kicked!")
+			parse("kick "..id.." \"You've begged for administration ranks, therefore you're punished with a kick!\"")
+		end
+	end
+end
+
+addhook("say","_say")
