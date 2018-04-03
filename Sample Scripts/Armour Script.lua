d62a4eeadf3dbda5a569c3858a55c655ec5513a1
Bișoc George <fraizeraust99@gmail.com>
2018-04-03 11:35:10 +0200

    Style changes only, nothing else

--- Sample Scripts/Armour Script.lua
+++ Sample Scripts/Armour Script.lua
@@ -1,37 +1,37 @@
 local Armour_player = {}
 
 function _serveraction(id, action)
-	if action == 1 then
-		menu(id,"Armours Menu, Light Armour, Armour, Heavy Armour, Medic Armour, Remove recent armour!")
+	if ( action == 1 ) then
+		menu(id, "Armours Menu, Light Armour, Armour, Heavy Armour, Medic Armour, Remove recent armour!")
 	end
 end
 
 
 function _menu(id, title, button)
-	if title == "Armours Menu" then
-		if button == 1 then
-			parse("equip "..id.." 79")
-			msg2(id,"\169152251152You're equipped with a Light armour!")
+	if ( title == "Armours Menu" ) then
+		if ( button == 1 ) then
+			parse("equip " .. id .. " 79")
+			msg2(id, "\169152251152You're equipped with a Light armour!")
 			Armour_player = nil
-		elseif button == 2 then
-			parse("equip "..id.." 80")
-			msg2(id,"\169152251152You're equipped with a Armour!")
+		elseif ( button == 2 ) then
+			parse("equip " .. id .. " 80")
+			msg2(id, "\169152251152You're equipped with a Armour!")
 			Armour_player = nil
-		elseif button == 3 then
-			parse("equip "..id.." 81")
-			msg2(id,"\169152251152You're equipped with a Heavy Armour!")
+		elseif ( button == 3 ) then
+			parse("equip " .. id .. " 81")
+			msg2(id, "\169152251152You're equipped with a Heavy Armour!")
 			Armour_player = nil
-		elseif button == 4 then
-			parse("equip "..id.." 82")
-			msg2(id,"\169152251152You're equipped with a Medic Armour")
+		elseif ( button == 4 ) then
+			parse("equip " .. id .. " 82")
+			msg2(id, "\169152251152You're equipped with a Medic Armour")
 			Armour_player = nil
-		elseif button == 5 then
-			parse("setarmor "..id.." 0")
-			msg2(id,"\169220200600Your recent armour has been removed!")
+		elseif ( button == 5 ) then
+			parse("setarmor " .. id .. " 0")
+			msg2(id, "\169220200600Your recent armour has been removed!")
 			Armour_player = nil
 		end
 	end
 end
 
-addhook("serveraction","_serveraction")
-addhook("menu","_menu")
+addhook("serveraction", "_serveraction")
+addhook("menu", "_menu")
