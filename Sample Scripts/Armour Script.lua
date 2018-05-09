6e2635a62c95e527183fe1a1c8182c13ac2c1b43
Bișoc George <fraizeraust99@gmail.com>
2018-05-09 18:55:15 +0200

    Convert tabs to spaces, space width to 2

--- Sample Scripts/Armour Script.lua
+++ Sample Scripts/Armour Script.lua
@@ -1,36 +1,36 @@
 local Armour_player = {}
 
 function _serveraction(id, action)
-	if ( action == 1 ) then
-		menu(id, "Armours Menu, Light Armour, Armour, Heavy Armour, Medic Armour, Remove recent armour!")
-	end
+  if ( action == 1 ) then
+    menu(id, "Armours Menu, Light Armour, Armour, Heavy Armour, Medic Armour, Remove recent armour!")
+  end
 end
 
 
 function _menu(id, title, button)
-	if ( title == "Armours Menu" ) then
-		if ( button == 1 ) then
-			parse("equip " .. id .. " 79")
-			msg2(id, "\169152251152You're equipped with a Light armour!")
-			Armour_player = nil
-		elseif ( button == 2 ) then
-			parse("equip " .. id .. " 80")
-			msg2(id, "\169152251152You're equipped with a Armour!")
-			Armour_player = nil
-		elseif ( button == 3 ) then
-			parse("equip " .. id .. " 81")
-			msg2(id, "\169152251152You're equipped with a Heavy Armour!")
-			Armour_player = nil
-		elseif ( button == 4 ) then
-			parse("equip " .. id .. " 82")
-			msg2(id, "\169152251152You're equipped with a Medic Armour")
-			Armour_player = nil
-		elseif ( button == 5 ) then
-			parse("setarmor " .. id .. " 0")
-			msg2(id, "\169220200600Your recent armour has been removed!")
-			Armour_player = nil
-		end
-	end
+  if ( title == "Armours Menu" ) then
+    if ( button == 1 ) then
+      parse("equip " .. id .. " 79")
+      msg2(id, "\169152251152You're equipped with a Light armour!")
+      Armour_player = nil
+    elseif ( button == 2 ) then
+      parse("equip " .. id .. " 80")
+      msg2(id, "\169152251152You're equipped with a Armour!")
+      Armour_player = nil
+    elseif ( button == 3 ) then
+      parse("equip " .. id .. " 81")
+      msg2(id, "\169152251152You're equipped with a Heavy Armour!")
+      Armour_player = nil
+    elseif ( button == 4 ) then
+      parse("equip " .. id .. " 82")
+      msg2(id, "\169152251152You're equipped with a Medic Armour")
+      Armour_player = nil
+    elseif ( button == 5 ) then
+      parse("setarmor " .. id .. " 0")
+      msg2(id, "\169220200600Your recent armour has been removed!")
+      Armour_player = nil
+    end
+  end
 end
 
 addhook("serveraction", "_serveraction")
--- Sample Scripts/Begging.lua
+++ Sample Scripts/Begging.lua
@@ -1,12 +1,12 @@
 local Begging = {"pls me mod", "i want adm pls"}
 
 function OnSayBegging(id, text)
-	for i = 1, #Begging do
-		if ( string.find(text,Begging[i]) ) then
-			msg("\169240128128" .. player(id, "name") .. " was begging for administration ranks. Kicked!")
-			parse("kick " .. id .. " \"You've begged for administration ranks, therefore you're punished with a kick!\"")
-		end
-	end
+  for i = 1, #Begging do
+    if ( string.find(text,Begging[i]) ) then
+      msg("\169240128128" .. player(id, "name") .. " was begging for administration ranks. Kicked!")
+      parse("kick " .. id .. " \"You've begged for administration ranks, therefore you're punished with a kick!\"")
+    end
+  end
 end
 
 addhook("say", "OnSayBegging")
